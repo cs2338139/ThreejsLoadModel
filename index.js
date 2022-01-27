f8bf00b3db2d2de2c6464b836dcfcf73ec9a2190
cs2338139 <cs2338139@gmail.com>
2022-01-27 11:43:27 +0800

    enter

---
+++ index.js
@@ -0,0 +1,53 @@
+// import { GLTFLoader } from './3rd-party/Threejs/examples/jsm/loaders/GLTFLoader.js';
+var scene, camera, renderer;
+var geometry, material, mesh;
+var light;
+
+init();
+animate();
+
+function init() {
+  scene = new THREE.Scene();
+
+  const loader = new THREE.GLTFLoader();
+  loader.load( 'scene.gltf', function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+  geometry = new THREE.IcosahedronGeometry(200, 1);
+  material = new THREE.MeshBasicMaterial({
+    color: 0xfffffff,
+    wireframe: false,
+    wireframeLinewidth: 2,
+  });
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+  );
+  camera.position.z = 500;
+
+  //   light = new THREE.DirectionalLight("rgb(255, 255, 255)", 1.2);
+  //   light.position.set(1, 1, 1);
+  //   scene.add(light);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  //   renderer.setClearColor("#ffffff");
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
